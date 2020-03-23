58e18ad6b6ef8c75f880d65f4e1f1c04372df918
haroonj <42788186+haroonj@users.noreply.github.com>
2020-03-23 16:44:55 +0200

    first commit

---
+++ lib/loading.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+//import 'weather_model.dart';
+//import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    //getLocationData();
+  }
+
+//  void getLocationData() async {
+//    //WeatherModel weatherModel = WeatherModel();
+////    var data = await weatherModel.getLocationWeather();
+////
+////    Navigator.push(context, MaterialPageRoute(builder: (context) {
+////      return LocationScreen(
+////        locationWeather: data,
+//      );
+//    }));
+//  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitFadingCircle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
